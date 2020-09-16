b86a3d9fd6dab4f5d5669b56eb985f86de588d5a
zura-t <66543460+zura-t@users.noreply.github.com>
2020-09-17 03:39:07 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+let fruits = [
+	{ id: 1, title: 'Яблоки', price: 20, img: 'https://e1.edimdoma.ru/data/ingredients/0000/2374/2374-ed4_wide.jpg?1487746348' },
+	{ id: 2, title: 'Апельсины', price: 30, img: 'https://fashion-stil.ru/wp-content/uploads/2019/04/apelsin-ispaniya-kg-92383155888981_small6.jpg' },
+	{ id: 3, title: 'Манго', price: 40, img: 'https://itsfresh.ru/upload/iblock/178/178d8253202ef1c7af13bdbd67ce65cd.jpg' }
+]
+
+const priceModal = $.modal({
+	title: 'Цена на товар',
+	closable: true,
+	width: '400px',
+	footerButtons: [
+		{
+			text: 'Закрыть', type: 'primary', handler() {
+				priceModal.close()
+			}
+		}
+	]
+})
+
+
+function createCards() {
+	const toHTML = obj => `
+	<div class="col">
+		<div class="card">
+			<img class="card-img-top" style="height: 300px;" src=${obj.img}>
+			<div class="card-body">
+				<h5 class="card-title">${obj.title}</h5>
+				<a href="#" class="btn btn-primary" data-btn="price" data-id="${obj.id}">Посмотреть цену</a>
+				<a href="#" class="btn btn-danger" data-btn="remove" data-id="${obj.id}">Удалить</a>
+			</div>
+		</div>
+	</div>
+	`
+
+	const html = fruits.map(toHTML).join('')
+	document.querySelector('.row').innerHTML = html
+}
+
+createCards()
+
+document.addEventListener('click', event => {
+	event.preventDefault()
+	const btnType = event.target.dataset.btn
+	const id = +event.target.dataset.id
+	const fruit = fruits.find(f => f.id === id)
+
+	if (btnType === 'price') {	
+		priceModal.setContent(`
+		<p>Цена на ${fruit.title}: <strong>${fruit.price}</strong></p>`)
+		priceModal.open()
+	} else if(btnType === 'remove') {
+		$.confirm({
+			title: 'Вы уверены',
+			content: `<p>Вы удаляете фрукт: <strong>${fruit.title}</strong></p>`
+		}).then(() => {
+			fruits = fruits.filter(f => f.id !== id)
+			createCards()
+		}).catch(() => {
+			console.log('cancel')
+		})
+	}
+})
